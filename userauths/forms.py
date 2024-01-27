2c92497d85e9b898cb0f7aa2461c1cebb436dc3e
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-01-27 18:57:49 +0100

    Add-funcionality-to-form

--- userauths/forms.py
+++ userauths/forms.py
@@ -6,6 +6,9 @@ class UserRegisterForm(UserCreationForm):
     full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Enter full name"}))
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Enter username"}))
     email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"example@gmail.com"}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"+383(49)123123"}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Enter password"}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Confirm password"}))
     class Meta:
         model = User
         fields = ['full_name', 'username', 'email', 'phone', 'password1', 'password2']
