c262bb2483f8a65a3f57f77ae9e1f498011bc8ee
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-01-28 23:42:10 +0100

    hotel-detail-view

--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
+from hotel.models import  Hotel, Room, Booking, RoomType
+
 def index(request):
-    return render(request, "hotel/hotel.html")
+    hotel = Hotel.objects.filter(status="Live")
+    context = {
+        "hotel":hotel
+    }
+    return render(request, "hotel/hotel.html", context)
+
+def hotel_detail(request, slug):
+    hotel = Hotel.objects.get(status="Live", slug=slug)
+    context = {
+        "hotel":hotel,
+    }
+    return render(request, "hotel/hotel_detail.html", context)
+
+# def room_type_detail(request, slug, rt_slug):
+#     hotel = Hotel.objects.get(status="Live", slug=slug)
+#     room_type = RoomType.objects.get(hotel=hotel, slug=rt_slug)
+#     rooms = Room.objects.filter(room_type=room_type, is_available=True)
+
+#     id = request.GET.get("hotel-id")
+#     adult = request.GET.get("adult")
+#     children = request.GET.get("children")
+#     room_type_ = request.GET.get("room-type")
+
