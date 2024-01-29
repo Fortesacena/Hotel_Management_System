7a0bb926114d4e6c92e7f465ebfee2c273aeebd6
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-01-29 20:20:05 +0100

    .

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ app_name = "booking"
 
 urlpatterns = [
     path("check_room_availability/", views.check_room_availability, name="check_room_availability"),
+    path("add_to_selection/", views.add_to_selection, name="add_to_selection"),
 ]
