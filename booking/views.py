6eef1d172d83114cb4d50751b2f393a6bec91cd8
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-01-29 18:40:01 +0100

    check_room_availability

--- booking/views.py
+++ booking/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
-from hotel.models import Hotel, Room, Booking, RoomServices, HotelGallery, HotelFeatures, RoomType
+from hotel.models import Hotel, Room, Booking, HotelGallery, HotelFeatures, RoomType
 
 def check_room_availability(request):
     if request.method == "POST":
@@ -14,3 +14,20 @@ def check_room_availability(request):
         adult = request.POST.get("adult")
         children = request.POST.get("children")
         room_type = request.POST.get("room-type")
+
+        hotel = Hotel.objects.get(id=id)
+        room_type = RoomType.objects.get(hotel=hotel, slug=room_type)
+
+        print("id ====", id)
+        print("room_type ====", room_type)
+        print("checkin ====", checkin)
+        print("checkout ====", checkout)
+        print("adult ====", adult)
+        print("children ====", children)
+        print("hotel ====", hotel)
+        print("room_type ====", room_type)
+
+        url = reverse("hotel:room_type_detail", args=[hotel.slug, room_type.slug])
+        url_with_params = f"{url}?hotel-id={id}&checkin={checkin}&checkout={checkout}&adult={adult}&children={children}&room_type={room_type}"
+        return HttpResponseRedirect(url_with_params)
+ 
--- hms_project/urls.py
+++ hms_project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("userauths.urls")),
     path("", include("hotel.urls")),
+    path("booking/", include("booking.urls")),
 ]
 
 
